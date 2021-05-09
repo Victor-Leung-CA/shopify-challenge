4d0a9cefb38e0aa671e7857a72207341029e3e66
Victor-Leung-CA <victor.leung@alumni.ubc.ca>
2021-05-09 00:48:00 -0700

    minor changes

--- src/api/getMovies.js
+++ src/api/getMovies.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 //Fetch movies from OMDb API
 const getMovies = async (searchInput) => {
     try {
-        const response = await axios.get('http://www.omdbapi.com/?apikey=' + process.env.API_KEY + '&s=' + searchInput);
+        let key = process.env.REACT_APP_API_KEY;
+        const response = await axios.get('https://www.omdbapi.com/?apikey=' + key + '&s=' + searchInput);
         console.log(response);
         return(response.data);
 
